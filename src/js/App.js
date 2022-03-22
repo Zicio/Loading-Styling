7e66fd9bd3a1bc344b9099bc6458662c407e491f
Zicio <zicio@yandex.ru>
2022-03-22 10:03:22 +0400

    Updated

--- src/js/App.js
+++ src/js/App.js
@@ -3,7 +3,7 @@ import Dom from './dom';
 export default class App {
   constructor(element) {
     this.element = element;
-    this.url = new URL('http://localhost:7000/'); // 'https://zicio-polling.herokuapp.com/'
+    this.url = new URL('https://zicio-sw.herokuapp.com/'); // 'http://localhost:7000/'
 
     App.start();
     this.getRequest();
@@ -26,7 +26,6 @@ export default class App {
     try {
       const response = await fetch(`${this.url}news`);
       if (response.ok) {
-        // OK return data
         const news = await response.json();
         Dom.showNews(news);
         return;
